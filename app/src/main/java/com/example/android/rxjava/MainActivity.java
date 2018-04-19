d6ed2b14c1f1fef1f480a3ecdefb52c73a974b2b
Brian Otieno <brianoti44@gmail.com>
2018-04-19 16:10:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/rxjava/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.android.rxjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+
+public class MainActivity extends AppCompatActivity {
+    TextView helloText;
+    RecyclerView recyclerView;
+    private LinearLayoutManager layoutManager;
+    private StockDataAdapter stockDataAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        helloText = findViewById(R.id.tv_hello);
+        recyclerView = findViewById(R.id.rv_stock_updates);
+
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        stockDataAdapter = new StockDataAdapter();
+        recyclerView.setAdapter(stockDataAdapter);
+
+        Observable.just("Hello! Please use this app responsibly")
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        helloText.setText(s);
+                    }
+                });
+    }
+
+}
